54edbfb812aab13f6230b2d73225703603a3fcc6
andresxDDDD <moraga_2006+1@hotmail.com>
2026-03-28 01:02:53 +0000

    ejemplos de clase

---
+++ clases/pilares.py
@@ -0,0 +1,18 @@
+#pilares de la poo
+
+#encapsulacion 
+class Cuenta_bancaria
+    def __init__(self,titular,saldo):
+        self.titular= titular
+        self.saldo= saldo
+
+    def depositar(self,monto)    
+
+        pass
+
+    def retirar():
+
+        pass
+
+cuenta1 = Cuenta_bancaria("juanito",10)
+cuenta2 = Cuenta_bancaria("marcela",100)
